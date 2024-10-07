f6d2839eddc0296205ee8ed97752916eeecc6857
Trong An <23020509@vnu.edu.vn>
2024-10-07 20:41:31 +0700

    3

---
+++ Fish.java
@@ -0,0 +1,10 @@
+public class Fish extends Animal{
+    public Fish(String name) {
+        super(name);
+    }
+
+    @Override
+    public boolean canSwim() {
+        return super.canSwim();
+    }
+}
