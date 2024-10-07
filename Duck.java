61d07124c5edc9b2822af39b6157722f139bd384
Trong An <23020509@vnu.edu.vn>
2024-10-07 20:40:58 +0700

    2

---
+++ Duck.java
@@ -0,0 +1,15 @@
+public class Duck extends Animal{
+    public Duck(String name) {
+        super(name);
+    }
+
+    @Override
+    public boolean canWalk() {
+        return super.canWalk();
+    }
+
+    @Override
+    public boolean canSwim() {
+        return super.canSwim();
+    }
+}
